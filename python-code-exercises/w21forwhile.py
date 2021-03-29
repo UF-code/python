20b76723cb8110bfce72dd7abea6abb16ee8680e
uf-code <ugur.firat.developer@gmail.com>
2021-03-29 18:16:01 +0300

    second solutions of howmanydigits

--- python-code-exercises/w21forwhile.py
+++ python-code-exercises/w21forwhile.py
@@ -5,3 +5,17 @@ def multiplicationTable(num):
     for i in range(10):
         print(f'{i} x {num} == {i*num}')
 
+
+
+#  Girilen bir sayının kaç basamaklı olduğunu belirleyen programı while döngüsü ile gerçekleştiriniz
+
+def howManyDigits(num):
+    return f'{num} number has {len(list(num))} digits'
+
+#alternative with while
+def howManyDigits2(num):
+    count=0
+    while(num > 0):
+        count += 1
+        num//= 10
+    return f'Number has {count} digits'
