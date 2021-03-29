eee84d00b1e70c9117d3880c71d918646473a997
uf-code <ugur.firat.developer@gmail.com>
2021-03-29 18:27:24 +0300

    numbers which can divisible to five

--- python-code-exercises/w21forwhile.py
+++ python-code-exercises/w21forwhile.py
@@ -16,6 +16,19 @@ def howManyDigits(num):
 def howManyDigits2(num):
     count=0
     while(num > 0):
-        count += 1
+        count+= 1
         num//= 10
     return f'Number has {count} digits'
+
+
+
+# Aşağıda bir listeye ait sayısal değerler verilmiştir. 
+# sayısalDeğerler = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+# Bu listedeki 5’e bölünen sayıları çıktı olarak veren programı hem for hem de while döngüsü ile 
+# gerçekleştiriniz. 150’den büyük değerleri dikkate almayınız
+
+def dividableToFive(list_of_numbers):
+    for num in list_of_numbers:
+        if(num <= 150 and n%5 == 0):
+            print(n)
+
