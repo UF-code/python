0ca70d6889b1eefc26a48956557842264f03c0a4
uf-code <ugur.firat.developer@gmail.com>
2021-03-30 21:43:42 +0300

    artificial neural network is created and plotted out

--- artificial-intelligence-examples/w41artificialNeuralNetworks.py
+++ artificial-intelligence-examples/w41artificialNeuralNetworks.py
@@ -33,3 +33,51 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2)
 from tensorflow.keras.utils import to_categorical
 y_train= to_categorical(y_train)
 y_test= to_categorical(y_test)
+
+
+
+# YSA modelinin olusturulmasi
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model= Sequential()
+model.add(Dense(16, input_dim=20, activation="relu"))
+model.add(Dense(12, activation="relu"))
+model.add(Dense(4, activation="softmax"))
+model.summary()
+
+
+# modelin derlenmsi
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics="accuracy")
+
+
+# modelin egitilmesi
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs= 50)
+
+
+# gerekli degerlerin gosterilmesi
+print('Ortalama egitim kaybi: ', np.mean(model.history.history["loss"]))
+print('Ortalama egitim basarimi: ', np.mean(model.history.history["accuracy"]))
+print('Ortalama dogrulama kaybi: ', np.mean(model.history.history["val_loss"]))
+print('Ortalama dogrulama basarimi: ', np.mean(model.history.history["val_accuracy"]))
+
+
+# egitim ve dogrulama basarimlarinin gosterilmesi
+import matplotlib.pyplot as plt 
+plt.plot(model.history.history["accuracy"])
+plt.plot(model.history.history["val_accuracy"])
+plt.title("Model Basarim")
+plt.ylabel(" Basarim")
+plt.xlabel("Epok")
+plt.legend(["Egitim", "Test"], loc="upper left")
+plt.show
+
+
+# egitim ve dogrulama basarimlarinin gosterilmesi
+plt.plot(model.history.history["loss"])
+plt.plot(model.history.history["val_loss"])
+plt.title("Model Kaybi")
+plt.ylabel(" Kaybi")
+plt.xlabel("Epok")
+plt.legend(["Egitim", "Test"], loc="upper left")
+plt.show
