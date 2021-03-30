af6c7728929e4ed1c1f8dc52eb829b2618872e95
uf-code <ugur.firat.developer@gmail.com>
2021-03-30 20:48:51 +0300

    load the datasets and deciding class numbers according to price range and standardization of price_range data's

--- artificial-intelligence-examples/w41artificialNeuralNetworks.py
+++ artificial-intelligence-examples/w41artificialNeuralNetworks.py
@@ -1,3 +1,24 @@
+# kutuphanelerin yuklenmesi
 import numpy as np 
 import pandas as pd 
-from sklearn.preprocessing import labelEncoder
+from sklearn.preprocessing import LabelEncoder
+
+
+# verisetlerin yuklenmesi
+data = pd.read_csv('phone_data.csv')
+
+
+# sinif sayisinin belirlenmesi
+label_encoder= LabelEncoder().fit(data.price_range)
+labels= label_encoder.transform(data.price_range)
+classes= list(label_encoder.classes_)
+
+X= data.drop(['price_range'], axis=1)
+y= labels
+
+
+
+# verilerin standartlastirilmasi
+from sklearn.preprocessing import StandardScaler 
+sc = StandardScaler()
+X= sc.fit_transform(X)
