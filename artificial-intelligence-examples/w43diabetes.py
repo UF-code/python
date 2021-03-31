b74bfb592969463df8bed2482c154bbb8f81e364
uf-code <ugur.firat.developer@gmail.com>
2021-03-31 23:07:33 +0300

    building ANN for diabetes data

--- artificial-intelligence-examples/w43diabetes.py
+++ artificial-intelligence-examples/w43diabetes.py
@@ -32,3 +32,19 @@ X_train, X_test, y_train, y_test= train_test_split(X, y, test_size= 0.2)
 from tensorflow.keras.utils import to_categorical
 y_train= to_categorical(y_train)
 y_test= to_categorical(y_test)
+
+
+# creating Artificial Neural Network for diabetes data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense 
+
+model= Sequential()
+model.add(Dense(8, input_dim=8, activation="sigmoid"))
+model.add(Dense(6,  activation="sigmoid"))
+model.add(Dense(4,  activation="sigmoid"))
+model.add(Dense(2,  activation="softmax"))
+model.summary()
+
+
+#  compiling model
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics="accuracy")
